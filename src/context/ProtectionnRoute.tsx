60cda9c0fc5baec9736b5757eeafcaaf3a2329ec
laiba-107 <alilaiba830@gmail.com>
2026-02-17 18:00:02 +0600

    added more files

---
+++ src/context/ProtectionnRoute.tsx
@@ -0,0 +1,36 @@
+import { ReactNode, useContext } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+
+type Props = {
+  requiredRole?: string | string[];
+  children?: ReactNode;
+};
+
+export default function ProtectedRoute({
+  requiredRole,
+  children,
+}: Props) {
+  const { auth } = useContext(AuthContext);
+
+  const isAuthenticated = auth.isAuthenticated && auth.token;
+
+  // 🚫 Not logged in → login page
+  if (!isAuthenticated) {
+    return <Navigate to="/admin/login" replace />;
+  }
+
+  // 🚫 Role check
+  if (requiredRole) {
+    const allowedRoles = Array.isArray(requiredRole)
+      ? requiredRole
+      : [requiredRole];
+
+    if (!auth.role || !allowedRoles.includes(auth.role)) {
+      return <Navigate to="/403" replace />;
+    }
+  }
+
+  // ✅ Authorized
+  return children ? <>{children}</> : <Outlet />;
+}
---
+++ src/pages/ForbiddenPage.tsx
@@ -0,0 +1,9 @@
+export default function ForbiddenPage() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-2xl font-semibold">
+        403 – You don’t have permission to access this page
+      </h1>
+    </div>
+  );
+}
